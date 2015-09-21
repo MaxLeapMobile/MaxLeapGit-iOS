ea201262b8001bca40562274b9fae102121815ad
夏军 <jxia@ilegendsoft.com>
2015-09-21 16:51:29 +0800

    creating project

---
+++ MaxLeapGit/MaxLeapGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MaxLeapGit
+//
+//  Created by Jun Xia on 15/9/21.
+//  Copyright (c) 2015年 iLegendsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MaxLeapGit/MaxLeapGitTests/MaxLeapGitTests.m
@@ -0,0 +1,40 @@
+//
+//  MaxLeapGitTests.m
+//  MaxLeapGitTests
+//
+//  Created by Jun Xia on 15/9/21.
+//  Copyright (c) 2015年 iLegendsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MaxLeapGitTests : XCTestCase
+
+@end
+
+@implementation MaxLeapGitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
