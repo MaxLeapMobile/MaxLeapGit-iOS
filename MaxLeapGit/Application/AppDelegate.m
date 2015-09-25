4b93128c776e5db7f64490d1a21d319187b3d880
夏军 <jxia@ilegendsoft.com>
2015-09-25 11:12:14 +0800

    support Travis CI and coveralls

---
+++ MaxLeapGit/Application/AppDelegate.m
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.m
+//  MaxLeapGit
+//
+//  Created by Jun Xia on 15/9/22.
+//  Copyright (c) 2015年 iLegendsoft. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self configureThirdPartySDK];
+    
+    return YES;    // Override point for customization after application launch.
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {}
+
+- (void)applicationWillTerminate:(UIApplication *)application {}
+
+- (void)configureThirdPartySDK {
+    [Flurry setAppVersion:kAppVersion];
+    [Flurry startSession:CONFIGURE(@"GitHub_Client_Secret")];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,2 @@
+pod 'MagicalRecord', '~> 2.3.0'
+pod 'AFNetworking', '~> 2.6.0'
